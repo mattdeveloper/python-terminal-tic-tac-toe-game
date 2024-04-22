5d3b2446c9b169c2572268e95b2261458cba19ce
Matt Oliveira <talk@mattdeveloper.com>
2024-04-22 20:20:17 -0300

    add check_win and play game

--- game.py
+++ game.py
@@ -27,8 +27,49 @@ def print_board(board):
         print("|" + "|".join(row) + "|")
 
 
-# Testing
-board = [[" " for _ in range(3)] for _ in range(3)]
-print_board(board)
-player_move(board, "X")
-print_board(board)
+def check_win(board):
+    # Check horizontal, vertical, and diagonal lines for a win
+    lines = []
+    lines.extend(board)  # horizontal lines
+    lines.extend(zip(*board))  # vertical lines
+    lines.append([board[i][i] for i in range(3)])  # main diagonal
+    lines.append([board[i][2 - i] for i in range(3)])  # secondary diagonal
+
+    for line in lines:
+        if line.count(line[0]) == 3 and line[0] != " ":
+            return True
+    return False
+
+
+def check_tie(board):
+    for row in board:
+        if " " in row:
+            return False
+    return True
+
+
+def play_game():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    while True:
+        print_board(board)
+        player_move(board, current_player)
+
+        if check_win(board):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        if check_tie(board):
+            print_board(board)
+            print("The game is a tie!")
+            break
+
+        # Switch player
+        current_player = "O" if current_player == "X" else "X"
+
+    print("Game over!")
+
+
+# Start the game
+play_game()
